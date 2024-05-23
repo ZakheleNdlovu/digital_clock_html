ec412bf06d772f579d06bf7f270cfc1e148ce5cf
Zakhele Ndlovu <134063053+ZakheleNdlovu@users.noreply.github.com>
2024-05-22 20:54:00 -0700

    commit no2

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ function tim(){
     let m = today.getMinutes()
     let s = today.getSeconds()
     let am_pm = h >= 12? 'pm' : 'am';
-    h = h<10 ? '0' + h : h
+    
     m = m<10 ? '0' + m : m
     s = s<10 ? '0' + s : s
      
+    h = (h % 12) || 12;
+    h = h<10 ? '0' + h : h
     time.innerHTML = h + ':' + m + ':' + s + ' ' + am_pm
 
 }
